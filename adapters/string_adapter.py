0e80ba73ea1a0c9f744e021e2874e274d48896bb
Maxime Multari <maxime.multari83@gmail.com>
2026-04-23 16:39:09 +0200

    Remove skipping creation of string file

--- adapters/string_adapter.py
+++ adapters/string_adapter.py
@@ -143,11 +143,11 @@ class StringAdapter:
         uniprot_mapping=get_data_frame_from_tsv_results(results_map)
         uniprot_mapping['To_OLN']=uniprot_mapping['To'].str.split('.').str.get(0)
         
-        # print('String proteins before filtering OLN:', uniprot_mapping['To_OLN'].unique().shape[0])
+        # logger.info('String proteins before filtering OLN:', uniprot_mapping['To_OLN'].unique().shape[0])
         
         uniprot_mapping_filtered=GenomeAdapter(self.genome_path).filter_input_genome(uniprot_mapping, 'To_OLN')
         
-        # print('String proteins after filtering OLN:', uniprot_mapping_filtered['To_OLN'].unique().shape[0])
+        # logger.info('String proteins after filtering OLN:', uniprot_mapping_filtered['To_OLN'].unique().shape[0])
         
         return uniprot_mapping_filtered
     
@@ -160,53 +160,55 @@ class StringAdapter:
         
         output_file='download/string/String_PPI_sly_filtered.txt'
         
-        if not os.path.exists(output_file):
-            logger.info("Creating file.")
-            # Generate genome_ids directly from the function
-            genome_ids = defaultdict(set)
-            for _, row in self._create_uniprot_mapping().iterrows():
-                from_id = row['From']
-                to_id = row['To_OLN']
-                genome_ids[from_id].add(to_id)
+        # if not os.path.exists(output_file):
+        logger.info("Creating file.")
+        # Generate genome_ids directly from the function
+        genome_ids = defaultdict(set)
+        for _, row in self._create_uniprot_mapping().iterrows():
+            from_id = row['From']
+            to_id = row['To_OLN']
+            genome_ids[from_id].add(to_id)
 
-            # Filter the STRING database file and add genome IDs as additional columns
-            seen_interactions = set()  # to keep track of unique interactions
-            with open(output_file, 'w') as filtered_file:
-                with open('download/string/4081.protein.links.v12.0.txt.gz.decomp', 'r') as string_file:
-                    header = next(string_file).strip()  # Read and store the header
-                    # Split the header into individual column names
-                    column_names = header.split()
-                    # Construct the new header with tabs as delimiters
-                    header_with_genome_ids = '\t'.join(column_names) + '\tOLN1\tOLN2\n'
-                    # Write the new header to the filtered file
-                    filtered_file.write(header_with_genome_ids)
+        # Filter the STRING database file and add genome IDs as additional columns
+        seen_interactions = set()  # to keep track of unique interactions
+        with open(output_file, 'w') as filtered_file:
+            with open('download/string/4081.protein.links.v12.0.txt.gz.decomp', 'r') as string_file:
+                header = next(string_file).strip()  # Read and store the header
+                # Split the header into individual column names
+                column_names = header.split()
+                # Construct the new header with tabs as delimiters
+                header_with_genome_ids = '\t'.join(column_names) + '\tOLN1\tOLN2\n'
+                # Write the new header to the filtered file
+                filtered_file.write(header_with_genome_ids)
 
-                    for line in string_file:
-                        protein1, protein2, score = line.strip().split()
-                        # Extract correct IDs from protein IDs in STRING file
-                        protein1_id = protein1.split('.')[1] if '.' in protein1 else protein1
-                        protein2_id = protein2.split('.')[1] if '.' in protein2 else protein2
-                        # Check if both protein1 and protein2 are in the OLN dict. We keep the interaction only if both are present.
-                        if protein1_id in genome_ids and protein2_id in genome_ids and int(score) > self.combined_score:  # combine score filter
-                            interaction = tuple(sorted((protein1_id, protein2_id)))  # sort to ensure uniqueness
-                            if interaction not in seen_interactions:
-                                seen_interactions.add(interaction)
-                                # Duplicate interactions for all combinations of genome IDs
-                                for genome1_id in genome_ids[protein1_id]:
-                                    for genome2_id in genome_ids[protein2_id]:
-                                        # Adjust delimiter to '\t' in the output line
-                                        line_with_genome_ids = f"{protein1}\t{protein2}\t{score}\t{genome1_id}\t{genome2_id}\n"
-                                        filtered_file.write(line_with_genome_ids)
+                count = 0
+                for line in string_file:
+                    count +=1
+                    protein1, protein2, score = line.strip().split()
+                    # Extract correct IDs from protein IDs in STRING file
+                    protein1_id = protein1.split('.')[1] if '.' in protein1 else protein1
+                    protein2_id = protein2.split('.')[1] if '.' in protein2 else protein2
+                    # Check if both protein1 and protein2 are in the OLN dict. We keep the interaction only if both are present.
+                    if protein1_id in genome_ids and protein2_id in genome_ids and int(score) > self.combined_score:  # combine score filter
+                        interaction = tuple(sorted((protein1_id, protein2_id)))  # sort to ensure uniqueness
+                        if interaction not in seen_interactions:
+                            seen_interactions.add(interaction)
+                            # Duplicate interactions for all combinations of genome IDs
+                            for genome1_id in genome_ids[protein1_id]:
+                                for genome2_id in genome_ids[protein2_id]:
+                                    # Adjust delimiter to '\t' in the output line
+                                    line_with_genome_ids = f"{protein1}\t{protein2}\t{score}\t{genome1_id}\t{genome2_id}\n"
+                                    filtered_file.write(line_with_genome_ids)
                 
-        else : 
-            logger.info(f"File '{output_file}' already exists. Skipping creation.")
+        # else : 
+        #     logger.info(f"File '{output_file}' already exists. Skipping creation.")
             
         string = pd.read_csv(output_file, sep='\t')
         
-        # print('String PPI interaction:', string[['OLN1','OLN2']].drop_duplicates().shape[0])
+        logger.info('String PPI interaction before filtering:', count) 
         
-        proteins = pd.concat([string['OLN1'], string['OLN2']])
+        # proteins = pd.concat([string['OLN1'], string['OLN2']])
         
-        # print('String proteins after mapping and filtering:', proteins.unique().shape[0])
+        logger.info('String proteins after mapping and filtering:', string[['OLN1','OLN2']].drop_duplicates().shape[0])
         
         return string
